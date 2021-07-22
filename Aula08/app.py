baee3051647a030be47b612f73b58eb4607d1e8a
Gabriel <gpereira@blueedtech.com.br>
2021-07-21 22:38:04 -0300

    Aula08 - Lista quase pronta

--- Aula08/app.py
+++ Aula08/app.py
@@ -1,21 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
+itens = []
 
 @app.route('/')
 def index():
-    nomelista = "Lista de coisas a fazer (dentro da var)"
-    listapronta = False
+    nomelista = "LISTA DE AFAZERES"
 
     return render_template(
         'index.html', 
         nomelista = nomelista, 
-        listapronta = listapronta
+        itens=itens
         )
    
-@app.route('/pagina2')
-def pagina2():
-    return '<h1>Esse é a página 2!!</h1>'
+# @app.route('/pagina2')
+# def pagina2():
+#     return '<h1>Esse é a página 2!!</h1>'
+
+@app.route('/nova', methods=['POST','GET'])
+def nova():
+
+    if request.method == 'POST':
+        item = request.form['item']
+        itens.append(item)
+        return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
